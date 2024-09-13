6567468b8fa2f8a2484285393cc9368414db32ac
Kavinkumar <kavinkumar1543884@gmail.com>
2024-09-14 00:00:56 +0530

    Final version

--- content.js
+++ content.js
@@ -1,12 +1,23 @@
 window.onload = function() {
-    const addButton = () => {
-        const commentSection = document.querySelector(
-            "#creation-box #labelAndInputContainer"
-        );
+    const addTimeStamp = () => {
         const inputField = document.querySelector(
             "#creation-box #labelAndInputContainer #contenteditable-root"
         );
-        if (commentSection && !document.querySelector("#timestampButton")) {
+        if (inputField) {
+            const video = document.querySelector("video");
+            const currentTime = video.currentTime;
+            let formattedTime = new Date(currentTime * 1000)
+                .toISOString()
+                .slice(11, 19);
+            const currentText = inputField.innerText;
+            if (currentText.length && currentText[currentText.length - 1] !== " ") {
+                formattedTime = " " + formattedTime;
+            }
+            inputField.innerText += formattedTime;
+        }
+    };
+    const addButton = (container, isPlaceholder = false) => {
+        if (container && !document.querySelector("#timestampButton")) {
             const button = document.createElement("button");
             button.id = "timestampButton";
             button.innerText = "00:12";
@@ -23,7 +34,7 @@ window.onload = function() {
                     flex-shrink: 0;
                     font-size: 100%;
                     margin-bottom: 5px;
-                    position: relative
+                    position: relative;
                 }
                 #timestampButton::after {
                     content: "+";
@@ -39,8 +50,7 @@ window.onload = function() {
                     color: #fff;
                     border: 1px solid #fff;
                 }
-                
-                #creation-box #labelAndInputContainer {
+                #creation-box #labelAndInputContainer, #simple-box #placeholder-area {
                     display: flex;
                     gap: 10px;
                     align-items: flex-start;
@@ -48,23 +58,31 @@ window.onload = function() {
             `;
             document.head.appendChild(style);
 
-            commentSection.prepend(button);
+            container.prepend(button);
 
             button.onclick = () => {
-                const video = document.querySelector("video");
-                const currentTime = video.currentTime;
-                const formattedTime = new Date(currentTime * 1000)
-                    .toISOString()
-                    .slice(11, 19);
-                inputField.innerText += formattedTime;
+                if (isPlaceholder) {
+                    setTimeout(() => addTimeStamp());
+                } else {
+                    addTimeStamp();
+                }
             };
         }
     };
 
-    const observer = new MutationObserver((_mutations, observer) => {
-        if (document.querySelector("#creation-box #labelAndInputContainer")) {
-            addButton();
-            observer.disconnect();
+    const observer = new MutationObserver(() => {
+        const placeholderComments = document.querySelector("#placeholder-area");
+        const comments = document.querySelector(
+            "#creation-box #labelAndInputContainer"
+        );
+        if (comments) {
+            const button = document.querySelector("#timestampButton");
+            if (button && placeholderComments.contains(button)) {
+                placeholderComments.removeChild(button); // Remove from placeholderComments if present
+            }
+            addButton(comments); // Add button to comments
+        } else if (placeholderComments) {
+            addButton(placeholderComments, true); // Add button to placeholderComments if it exists
         }
     });
 
